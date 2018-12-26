2a526d1214227c8d8b3c9f08725bc4cd9a3771c1
winnie <wangliu023@qq.com>
2018-12-26 10:56:11 +0800

    sync

---
+++ app/src/main/java/com/winnie/views/timepiker/TimePickerActivity.java
@@ -0,0 +1,23 @@
+package com.winnie.views.timepiker;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * @author winnie
+ */
+public class TimePickerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void pickTime(View view) {
+    }
+
+    public void pickDate(View view) {
+    }
+}
